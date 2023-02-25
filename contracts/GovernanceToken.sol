1b3eae03f5b040e0eb075ab27ccf95896e6c3fdd
Montywilliams <3650@holbertonschool.com>
2023-02-25 07:01:38 -0600

    corected typo in contract declaration and switched from ERC20 to ERC20Votes: this contract has added functionality which we use to take a snap shot off the block chain: it also requires us to add an additional constructor for the ERC20Permit contract

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract GovernanceToken {
+contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000;
 
-    constructor() ERC20("GovernanceToken", "GT") {
+    constructor()
+        ERC20("GovernanceToken", "GT")
+        ERC20Permit("GovernanceToken")
+    {
         _mint(msg.sender, s_maxSupply);
     }
 }
