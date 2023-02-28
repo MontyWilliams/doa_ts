6292f43d5d6b662fd9fa29083807b92ee4446e07
Montywilliams <3650@holbertonschool.com>
2023-02-28 08:44:52 -0600

    creat timelock file

---
+++ deploy/02-deploy-time-lock.ts
@@ -0,0 +1,17 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+import {MIN_DELAY} from "../helper-hardhat-config"
+
+const deployTimeLock: DeployDunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {getNamedAccounts, deployments} = hre;
+    const {deploy, log} = deployments;
+    const {deployer} = await getNamedAccounts(); 
+    log("Deploying TimeLock...");
+
+    const timeLock = await deploy("TimeLock",) {
+        from: deployer,
+        args: [MIN_DELAY]
+    }
+
+
+}
