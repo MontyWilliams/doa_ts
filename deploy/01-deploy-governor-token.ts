29121e6a16adae84305297f774f30f4630795f0b
Montywilliams <3650@holbertonschool.com>
2023-02-28 03:23:29 -0600

    import ether from hardhat: create delegate function that uses ethers to get contract info and then awaits all delegated transactions: it basically counts the votes

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,10 +1,24 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
+import {ethers} from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
 ) {
     const {getNamedAccounts, deployments, network } = hre;
+    const {deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    log("Deploying Governance Token...");
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+    log(`Deployed governance token to address ${governanceToken.address} Bro Bro`)
+};
+
+const delegate = async (governanceTokenAddrsss: string, deletedAccount: string) => {
+    const governanceToken = await ethers.getContracdtAt("GovernaceToken", governanceTokenAddrsss);
 }
 
 export default deployGovernanceToken
