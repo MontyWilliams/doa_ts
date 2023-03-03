1aaf505901d5648e3ebffcbbeaf760b3e2b59ef5
Montywilliams <3650@holbertonschool.com>
2023-03-03 10:27:59 -0600

    crate proposal script: import ethers from hardhat: get Box and GevernorContract using ehters

---
+++ scripts/propose.ts
@@ -0,0 +1,6 @@
+import { ethers } from 'hardhat'
+
+export async function propose() {
+    const governor = await ethers.getContract("GovernorContracts");
+    const box = await ethers.getContract("Box");
+}
