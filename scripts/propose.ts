c68911a55952651b958c49c6d4fc62515ee69aba
Montywilliams <3650@holbertonschool.com>
2023-03-03 10:34:14 -0600

    create encoded function call that interacts directly with the abi in the EVM, because it will act autonomosly we encode tha values so that it can remain autonomous

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,6 +1,10 @@
 import { ethers } from 'hardhat'
 
-export async function propose() {
+export async function propose(args: any[], functionToCall: string) {
     const governor = await ethers.getContract("GovernorContracts");
     const box = await ethers.getContract("Box");
+    const encodedFuncitonCall = box.interface.encodeFunctionData(
+        functionToCall,
+        args
+    )
 }
