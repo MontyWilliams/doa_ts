d72a8c4606c8b63befc6f5c51ec470192b4e129e
Montywilliams <3650@holbertonschool.com>
2023-03-03 07:00:30 -0600

    export function: correct syntax

--- deploy/05-deploy-contract.ts
+++ deploy/05-deploy-contract.ts
@@ -2,12 +2,12 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat/types"
 import { ethers } from "hardhat"
 
-const deployContract: DeployerFunction = async function (
+const deployContract: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
 ) {
     // @ts-ignore
     const { getNamedAccounts, deployments } = hre;
-    const { deploy, log, get } = deployoments;
+    const { deploy, log, get } = deployments;
     const { deployer } = await getNamedAccounts();
     log("Deploying contract....")
     const box = await deploy("Box", {
@@ -15,9 +15,10 @@ const deployContract: DeployerFunction = async function (
         args: [],
         log: true,
     })
+    const boxContract = await ethers.getContract("Box", deployer)
     const timeLock = await ethers.getContract("TimeLock");
-    const boxContract = await ethers.getContract("Box, box.address")
     const transferOwnerTx = await boxContract.transferOwnership(timeLock.address);
     await transferOwnerTx.wait(1);
     log("Congradulations all that sh*& works")
 }
+export default deployContract
