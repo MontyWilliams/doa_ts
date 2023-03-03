1f6a5eeee5ed03f67bf5ce2ad9ffc23adbd12e30
Montywilliams <3650@holbertonschool.com>
2023-03-03 10:43:12 -0600

    import env variables: pass variables to propose fuction to update the contract with the new proposal to change the value

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'hardhat'
+import { FUNC, NEW_STORE_VALUE } from '../helper-hardhat-configuration.ts'
 
 export async function propose(args: any[], functionToCall: string) {
     const governor = await ethers.getContract("GovernorContracts");
@@ -6,5 +7,13 @@ export async function propose(args: any[], functionToCall: string) {
     const encodedFuncitonCall = box.interface.encodeFunctionData(
         functionToCall,
         args
-    )
+    );
+    console.log(encodedFunctionCall)
 }
+
+propose([NEW_STORE_VALUE], FUNC)
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
