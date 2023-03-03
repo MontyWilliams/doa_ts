a1c6d6732208d5545011aace08dc6143655fe0d3
Montywilliams <3650@holbertonschool.com>
2023-03-03 06:33:07 -0600

    create script to deploy box contract

--- deploy/05-deploy-contract.ts
+++ deploy/05-deploy-contract.ts
@@ -9,4 +9,10 @@ const deployContract: DeployerFunction = async function (
     const { deploy, log, get } = deployoments;
     const { deployer } = await getNamedAccounts();
     log("Deploying contract....")
+    const box = await deploy("Box", {
+        from: deployer,
+        args: [],
+        log: true,
+    })
+    
 }
