290985018c88b6b2467e5d38304e4fe1b6237874
Montywilliams <3650@holbertonschool.com>
2023-03-03 06:14:08 -0600

    import ADDRESS_ZERO from helper file: grant the proposal power to governor contract: give everyone executor permision by setting it to address 0x000: revoke the admin role from deployer once other roles are set up so now the app isnt owned by it and is decentrilized

--- deploy/04-setup-governor-contract.tsx
+++ deploy/04-setup-governor-contract.tsx
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironmrnt } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-implort { ethers} from "ethers"
+import { ethers} from "ethers"
+import { ADDRESS_ZERO } from "../helper-hardhat-config.ts"
 
 const setupContracts: DeployFunction = async Function (
     hre: HrdhatRuntimeEnviornment
@@ -15,6 +16,12 @@ const setupContracts: DeployFunction = async Function (
     const proposerRole = await timeLock.PROPOSER_ROLE();
     const executorRole = await timeLock.EXECUTOR_ROLE();
     const adminRole = await timeLock.TIMELOCK_ADMIN_ROLE();
-    
+
+    const proposerTx = await timeLock.grantRole(proposerRole, governor.address);
+    await proposerTx.wait(1);
+    const executorTx = await timeLock.grantRole(executorRole, ADDRESS_ZERO);
+    await executorTx.wait(1);
+    const revokeTx = await timeLock.revokeRole(adminRole, deployer);
+    await revokeTx.wait(1);
     
 };
