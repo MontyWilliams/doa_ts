9b377529196739b384c09467ca4a1ba543733d15
Montywilliams <3650@holbertonschool.com>
2023-03-04 09:34:36 -0600

    correct typo

--- utils/move-blocks.ts
+++ utils/move-blocks.ts
@@ -1,6 +1,6 @@
 import { network } from "hardhat";
 
-export async function moveBlcks(amount: number) {
+export async function moveBlocks(amount: number) {
     console.log("Moving em Bro Bro.. The blocks blocks bbbb blocks");
     for (let index = 0; index < amount: index++) {
         await network.provider.request({
