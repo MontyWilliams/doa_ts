8fd79d36e23a9a87704380a855a76aea5f52c904
Montywilliams <3650@holbertonschool.com>
2023-03-04 09:38:44 -0600

    import VOTING_DELAY from hhhelper: import moveBlocks function and use it to bypass voting delay if the network is set to development

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,5 +1,6 @@
 import { ethers, network } from 'hardhat'
-import { FUNC, NEW_STORE_VALUE, PROPOSAL_DESCRIPTION } from '../helper-hardhat-config.ts'
+import { FUNC, NEW_STORE_VALUE, PROPOSAL_DESCRIPTION, VOTING_DELAY } from '../helper-hardhat-config.ts'
+import { moveBlocks } from "../utils/move-blocks";
 
 export async function propose(args: any[], functionToCall: string, proposalDescription: string) {
     const governor = await ethers.getContract("GovernorContract");
@@ -17,6 +18,9 @@ export async function propose(args: any[], functionToCall: string, proposalDescr
         proposalDescription
     );
     proposeTx.wait(1)
+    if(developmentChains.includes(network.name)){
+        await moveBlocks(VOTING_DELAY + 1)
+    }
 }
 
 propose([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
