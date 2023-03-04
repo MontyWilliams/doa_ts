b6fa8829b05ed130e4ce44efa0b9c730c587a004
Montywilliams <3650@holbertonschool.com>
2023-03-04 09:19:44 -0600

    import proposal des from hhhelper:log value and pass it to propose function

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,17 +1,19 @@
 import { ethers } from 'hardhat'
-import { FUNC, NEW_STORE_VALUE } from '../helper-hardhat-config.ts'
+import { FUNC, NEW_STORE_VALUE, PROPOSAL_DESCRIPTION } from '../helper-hardhat-config.ts'
 
-export async function propose(args: any[], functionToCall: string) {
+export async function propose(args: any[], functionToCall: string, proposalDescription: string) {
     const governor = await ethers.getContract("GovernorContract");
     const box = await ethers.getContract("Box");
     const encodedFunctionCall = box.interface.encodeFunctionData(
         functionToCall,
         args
     );
-    console.log(encodedFunctionCall)
+    console.log(`Proposing ${functionToCall} on ${box.address} with ${args}`)
+    console.log(`Proposal Description \n ${proposalDescription}  `)
+    
 }
 
-propose([NEW_STORE_VALUE], FUNC)
+propose([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
     .then(() => process.exit(0))
     .catch((error) => {
         console.log(error);
