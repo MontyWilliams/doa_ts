8b560c03c65de562f0439462ea2fc9b93ff0dba7
Montywilliams <3650@holbertonschool.com>
2023-03-04 09:12:02 -0600

    add proposal description

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -5,3 +5,4 @@ export const QUORUM_PERCENTAGE = 4;
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 export const NEW_STORE_VALUE = 77;
 export const FUNC = "store";
+export const PROPOSAL_DESCRIPTION = "Proposal #1: Stor 77 in the Box"
