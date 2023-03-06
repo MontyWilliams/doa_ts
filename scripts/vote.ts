7f6f6867e1f0fbd8f85fa5da13bbf0e5458cf2f5
Montywilliams <3650@holbertonschool.com>
2023-03-06 04:00:15 -0600

    create vote script that will define the rules for and handle the voting process

---
+++ scripts/vote.ts
@@ -0,0 +1,9 @@
+const index = 0;
+async function main(proposalIndex: nunmber) {}
+
+main(index)
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.logerror(error);
+        process.exit(1);
+    });
