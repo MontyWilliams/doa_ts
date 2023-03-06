d2a2f6d1ff0019a7fcfdb7ca928440784f1b3eb0
Montywilliams <3650@holbertonschool.com>
2023-03-06 04:25:02 -0600

    import ethers from hh: get Gov contract from ethers and save it into var governor

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,11 +1,16 @@
 import * as fs from 'fs';
 import { proposalsFile } from '../helper-hardhat-config'
-import { network } from 'hardhat'
+import { network, ethers } from 'hardhat'
 
 const index = 0;
 async function main(proposalIndex: nunmber) {
     const proposals = JSON.parse(fs.readFileSync(proposalsFile, 'utf8'));
     const proposalsId = proposals[network.config.chainId!][proposalIndex];
+    // 0 = Against, 1 = For, 2 = Abstain
+    const voteWay = 1;
+    const governor = await ethers.getContract("GovernorContract")
+    const voteTxResponse = await governor.cast;
+
 }
 
 main(index)
