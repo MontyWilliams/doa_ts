1650323c594879f5a228a2629b5d50e4af23b3ca
Montywilliams <3650@holbertonschool.com>
2023-03-06 04:13:22 -0600

    import propoadlFile, fs, network create proposal var from reading proposals file

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+import { proposalsFile } from '../helper-hardhat-config'
+import { network } from 'hardhat'
+
 const index = 0;
-async function main(proposalIndex: nunmber) {}
+async function main(proposalIndex: nunmber) {
+    const proposal = JSON.parse(fs.readFileSync(proposalsFile, 'utf8'));
+
+}
 
 main(index)
     .then(() => process.exit(0))
