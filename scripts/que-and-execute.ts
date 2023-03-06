87853083086926e2ee8040d722282b2efcf95f64
Montywilliams <3650@holbertonschool.com>
2023-03-06 09:56:15 -0600

    import FUNC, NEW_STORE_VALUE, PROPOSAL_DESCRIPTION from hh: get the description hash, box contract and arguments from imported varials: creat descriptionHash var by using bytes conversion on PROPOSAL_DESCRIPTION

--- scripts/que-and-execute.ts
+++ scripts/que-and-execute.ts
@@ -1,5 +1,13 @@
-export async function queueAndExecute(){
+import { FUNC, NEW_STORE_VALUE, PROPOSAL_DESCRIPTION } from "../helper-hardhat-config";
 
+
+export async function queueAndExecute(){
+    const args = [NEW_STORE_VALUE];
+    const box = await ethers.getContract("Box");
+    const encodedFunctionCall = box.interface.encodeFunctonData(FUNC, args);
+    const descriptionHash = ethers.utils.keccack256(
+        ethers.utils.toUtf8Bytes(PROPOSAL_DESCRIPTION)
+    )
 }
 
 queueAndExecute(index)
